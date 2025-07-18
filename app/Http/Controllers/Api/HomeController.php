05856774f41671ca6ab4ebb31ac890ac5fb72f97
heita <heiheydesu@gmail.com>
2025-07-18 13:58:06 +0900

    homecontroller

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Controllers\Api\RankingController;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -10,9 +14,31 @@ class HomeController extends Controller
     public function index()
     {
         // TODO: ランディングページ用の集約データ取得処理
+        $rankingController = new RankingController();
+        $topRanked = $rankingController->index(new Request());
+        $trending = $this->getTrendingProducts();
         return response()->json([
             'message' => 'Aggregated home data',
-            'data' => [] // 集約データ
+            'data' => [
+                'topRanked' => [
+                    'items' => $topRanked,
+                    'total' => $topRanked->count(),
+                ],
+                'trending' => [
+                    'items' => $trending,
+                    'total' => $trending->count(),
+                ]
+            ]
         ]);
+    }
+    public function getTrendingProducts()
+    {
+        $resentWeeks = Carbon::now()->subWeeks(4);
+        return Product::with('categories')
+            ->where('created_at', '>=', $resentWeeks)
+            ->where('rating', '>=', 3.5)
+            ->orderByRaw('(rating * 0.4 + (download_count / 1000) * 0.6) DESC')
+            ->limit(10)
+            ->get();
     }
 }
